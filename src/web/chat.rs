51e38bed4d18d97973b1b239fd526f34fec6e23a
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2025-03-09 11:45:05 +0800

    Add Users

--- src/web/chat.rs
+++ src/web/chat.rs
@@ -1,40 +1,69 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
 use axum::{
     Router,
-    extract::{ConnectInfo, WebSocketUpgrade},
+    extract::{ConnectInfo, State, WebSocketUpgrade, ws},
     response::IntoResponse,
     routing,
 };
+use dashmap::DashMap;
 use futures_util::{SinkExt, stream::StreamExt};
+use tokio::sync::broadcast::{Sender, channel};
 
 use crate::error::Result;
 
-pub fn router() -> Router {
-    Router::new().route("/chat", routing::any(chat))
+#[derive(Debug)]
+enum Message {
+    Content(String),
 }
 
-async fn chat(ws: WebSocketUpgrade, info: ConnectInfo<SocketAddr>) -> Result<impl IntoResponse> {
-    println!(
-        "[{:^12}] - socket {}:{} connect",
-        "WebSocket",
-        info.ip(),
-        info.port()
-    );
+type Users = DashMap<SocketAddr, Sender<Arc<Message>>>;
+
+pub fn router() -> Router {
+    let users = Arc::new(Users::new());
+
+    Router::new()
+        .route("/chat", routing::any(chat))
+        .with_state(users)
+}
+
+async fn chat(
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    State(users): State<Arc<Users>>,
+) -> Result<impl IntoResponse> {
+    println!("[{:^12}] - socket {} connect", "WebSocket", addr);
 
     Ok(ws.on_upgrade(move |socket| async move {
         let (mut tx, mut rx) = socket.split();
+        let (sender, mut receiver) = channel::<Arc<Message>>(128);
 
-        while let Some(Ok(msg)) = rx.next().await {
-            if tx.send(msg).await.is_err() {
-                println!(
-                    "[{:^12}] - socket {}:{} disconnect\n",
-                    "WebSocket",
-                    info.ip(),
-                    info.port()
-                );
-                break;
-            };
-        }
+        users.insert(addr, sender);
+
+        let users = users.clone();
+        tokio::spawn(async move {
+            while let Some(Ok(message)) = rx.next().await {
+                let message = match message {
+                    ws::Message::Text(text) => text.to_string(),
+                    _ => continue,
+                };
+
+                let message = Arc::new(Message::Content(message));
+
+                users.iter().for_each(|user| {
+                    user.value().send(message.clone()).unwrap();
+                });
+            }
+        });
+
+        tokio::spawn(async move {
+            while let Ok(message) = receiver.recv().await {
+                let message = match &*message {
+                    Message::Content(content) => ws::Message::Text(content.into()),
+                };
+
+                tx.send(message).await.unwrap();
+            }
+        });
     }))
 }
